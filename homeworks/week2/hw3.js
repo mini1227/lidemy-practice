aab87a08fe603dc687e59073461791492446630e
D0511189 <alysa8612276161911@gmail.com>
2021-09-07 02:25:46 +0800

    week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+    var returnStr = '';
+    var strTemp = str.split('');
+    for (var i = str.length; i > 0; i--) {
+        returnStr = returnStr + strTemp[i - 1];
+    }
+    console.log(returnStr);
 }
 
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,21 @@
 function join(arr, concatStr) {
-  
+    var returnStr = '';
+    for (var i = 1; i <= arr.length; i++) {
+        returnStr = returnStr + arr[i - 1].toString();
+        if (i !== arr.length) {
+            returnStr = returnStr + concatStr.toString();
+        }
+    }
+    return returnStr;
 }
 
 function repeat(str, times) {
-  
+    var returnStr = '';
+    for (i = 0; i < times; i++) {
+        returnStr = returnStr + str;
+    }
+    return returnStr;
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join([1, 2, 3], ''));
+console.log(repeat('yoyo', 5));
